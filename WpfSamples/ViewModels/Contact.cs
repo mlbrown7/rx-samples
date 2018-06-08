5b6a2112bbd8e19e3e28baac814ce231abe4ae1e
matt <matt@welldatalabs.com>
2018-06-08 10:32:19 -0600

    validation

---
+++ WpfSamples/ViewModels/Contact.cs
@@ -0,0 +1,144 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfSamples.ViewModels
+{
+    public class Contact : ReactiveObject, INotifyDataErrorInfo
+    {
+        public Contact(int id, string name, int age)
+        {
+            IsEditing = false;
+            IsNotEditing = true;
+
+            //set properties
+            Id = id;
+            Name = name;
+            NameEdit = name;
+            Age = age;
+            AgeEdit = age;
+
+            PropertyErrors = new ConcurrentDictionary<string, List<string>>();
+        }
+
+        private int _id;
+        public int Id
+        {
+            get => _id;
+            set => this.RaiseAndSetIfChanged(ref _id, value);
+        }
+
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set => this.RaiseAndSetIfChanged(ref _name, value);
+        }
+
+        private string _nameEdit;
+        public string NameEdit
+        {
+            get => _nameEdit;
+            set => this.RaiseAndSetIfChanged(ref _nameEdit, value);
+        }
+
+        private int _age;
+        public int Age
+        {
+            get => _age;
+            set => this.RaiseAndSetIfChanged(ref _age, value);
+        }
+
+        private int _ageEdit;
+        public int AgeEdit
+        {
+            get => _ageEdit;
+            set => this.RaiseAndSetIfChanged(ref _ageEdit, value);
+        }
+
+        private bool _isEditing;
+        public bool IsEditing
+        {
+            get => _isEditing;
+            set => this.RaiseAndSetIfChanged(ref _isEditing, value);
+        }
+
+        private bool _isNotEditing;
+        public bool IsNotEditing
+        {
+            get => _isNotEditing;
+            set => this.RaiseAndSetIfChanged(ref _isNotEditing, value);
+        }
+
+
+        public void Validate()
+        {
+            ValidateNameEdit(this);
+        }
+
+
+        //Validation Methods
+
+        
+
+
+        private void ValidateNameEdit(Contact contact)
+        {
+            var errors = new List<string>();
+            if (string.IsNullOrWhiteSpace(contact.NameEdit))
+            {
+                errors.Add("Name is required.");
+            }
+            else
+            {
+                if (contact.NameEdit.Length < 3)
+                {
+                    errors.Add("Name must be at least 3 characters long.");
+                }
+                if (contact.Name.Length > 20)
+                {
+                    errors.Add("Name cannot be more than 20 characters long.");
+                }
+            }
+            PropertyErrors[nameof(contact.NameEdit)] = errors;
+            ErrorsChanged(this, new DataErrorsChangedEventArgs(nameof(contact.NameEdit)));
+        }
+
+
+        //INotifyDataErrorInfo
+
+        public bool HasErrors => PropertyErrorsExist();
+
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged = delegate { };
+
+        public IEnumerable GetErrors(string propertyName)
+        {
+            if (string.IsNullOrWhiteSpace(propertyName)) { return null;  }
+
+            if (PropertyErrors != null && PropertyErrors.ContainsKey(propertyName))
+            {
+                return PropertyErrors[propertyName];
+            }
+            return null;
+        }
+
+        private ConcurrentDictionary<string, List<string>> PropertyErrors { get; set; }
+
+
+        private bool PropertyErrorsExist()
+        {
+            if (PropertyErrors != null && PropertyErrors.Any(p => p.Value != null && p.Value.Count > 0))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
--- WpfSamples/ViewModels/GridEditSampleViewModel.cs
+++ WpfSamples/ViewModels/GridEditSampleViewModel.cs
@@ -1,7 +1,10 @@
 ﻿using ReactiveUI;
 using System;
+using System.Collections;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,7 +18,7 @@ namespace WpfSamples.ViewModels
         {
             Contacts = new ReactiveList<Contact> { ChangeTrackingEnabled = true };
             Contacts.AddRange(GetContactList());
-
+           
             EditContactCommand = ReactiveCommand.Create<Contact>((contact) =>
             {
                 contact.IsEditing = true;
@@ -30,16 +33,22 @@ namespace WpfSamples.ViewModels
 
             SaveContactCommand = ReactiveCommand.Create<Contact>((contact) =>
             {
-                contact.Name = contact.NameEdit;
-                contact.Age = contact.AgeEdit;
-                MessageBox.Show($"Saved changes for {contact.Name}");
+                contact.Validate();
+                
+                if (!contact.HasErrors)
+                {
+                    contact.Name = contact.NameEdit;
+                    contact.Age = contact.AgeEdit;
+                    MessageBox.Show($"Saved changes for {contact.Name}");
 
-                contact.IsEditing = false;
-                contact.IsNotEditing = true;
+                    contact.IsEditing = false;
+                    contact.IsNotEditing = true;
+                }
             });
         }
 
         ReactiveList<Contact> _contacts;
+
         public ReactiveList<Contact> Contacts
         {
             get => _contacts;
@@ -49,6 +58,7 @@ namespace WpfSamples.ViewModels
         public ReactiveCommand EditContactCommand { get; set; }
         public ReactiveCommand CancelEditContactCommand { get; set; }
         public ReactiveCommand SaveContactCommand { get; set; }
+
 
         private List<Contact> GetContactList()
         {
@@ -61,69 +71,4 @@ namespace WpfSamples.ViewModels
         }
     }
 
-
-    public class Contact : ReactiveObject
-    {
-        public Contact(int id, string name, int age)
-        {
-            IsEditing = false;
-            IsNotEditing = true;
-
-            //set properties
-            Id = id;
-            Name = name;
-            NameEdit = name;
-            Age = age;
-            AgeEdit = age;
-        }
-
-        private int _id;
-        public int Id
-        {
-            get => _id;
-            set => this.RaiseAndSetIfChanged(ref _id, value);
-        }
-
-        private string _name;
-        public string Name
-        {
-            get => _name;
-            set => this.RaiseAndSetIfChanged(ref _name, value);
-        }
-
-        private string _nameEdit;
-        public string NameEdit
-        {
-            get => _nameEdit;
-            set => this.RaiseAndSetIfChanged(ref _nameEdit, value);
-        }
-
-        private int _age;
-        public int Age
-        {
-            get => _age;
-            set => this.RaiseAndSetIfChanged(ref _age, value);
-        }
-
-        private int _ageEdit;
-        public int AgeEdit
-        {
-            get => _ageEdit;
-            set => this.RaiseAndSetIfChanged(ref _ageEdit, value);
-        }
-
-        private bool _isEditing;
-        public bool IsEditing
-        {
-            get => _isEditing;
-            set => this.RaiseAndSetIfChanged(ref _isEditing, value);
-        }
-
-        private bool _isNotEditing;
-        public bool IsNotEditing
-        {
-            get => _isNotEditing;
-            set => this.RaiseAndSetIfChanged(ref _isNotEditing, value);
-        }
-    }
 }
